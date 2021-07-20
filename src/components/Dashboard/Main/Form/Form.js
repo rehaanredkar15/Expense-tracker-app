b487d68e648db94717b658ec49bc346ec39f8de3
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-07-20 21:54:20 +0530

    feat - adding the form on the main component

---
+++ src/components/Dashboard/Main/Form/Form.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { TextField,Typography,Grid,Button,FormControl,InputLabel,Select,MenuItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+     
+   radioGroup:{
+      
+      display:'flex',
+      justifyContent:'center',
+      marginBottom:'-10px',
+   },
+   button:{
+       marginTop:'20px',
+
+   },
+
+}));
+
+
+const Form = () => {
+
+    const classes = useStyles();
+
+    return (
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography align="center" variant="subtitle2" gutterBottom>
+             ...
+            </Typography>
+          </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel>Type</InputLabel>
+                <Select> 
+                  <MenuItem value="Income">Income</MenuItem>
+                  <MenuItem value="Expense">Expense</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel>Category</InputLabel>
+                <Select> 
+                  <MenuItem value="Grocery">Grocery</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField type="number" label="Amount" fullWidth/>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField type="date" label="Date" fullWidth/>
+            </Grid>
+            <Button variant="outlined" color="primary" className={classes.button} fullWidth >Create</Button>
+        </Grid>
+    )
+}
+
+export default Form
