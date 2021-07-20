69307f30f887ff8456ba365788d4d396d35581ff
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-07-20 21:51:48 +0530

    fix - fixing the dashboard for the display of data

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Info from '../Dashboard/Info';
+import Main from '../Dashboard/Main/Main';
+import Form from '../Dashboard/Main/Form/Form';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+     
+       desktop: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  mobile: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  main: {
+    [theme.breakpoints.up('sm')]: {
+      paddingBottom: '5%',
+    },
+  },
+  last: {
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: theme.spacing(3),
+      paddingBottom: '200px',
+    },
+  },
+  grid: {
+    '& > *': {
+      margin: theme.spacing(4),
+      
+    },
+  },
+}));
+
+const Dashboard = () => {
+
+     const classes = useStyles();
+    
+    return (
+
+        <div>
+         <Grid className={classes.grid} container spacing={0} alignItems="center"  spacing={2} justify="center" style={{ height:'100vh' ,width:'100%'}}>
+            <Grid item xs={12} sm={4}>
+              <Main/>
+             </Grid>
+           <Grid item xs={12} sm={3} >
+              <Info title="Income"/>
+           </Grid>
+            <Grid item xs={12} sm={3}>
+              <Info title="Expenses"/>
+           </Grid>
+         </Grid>
+        </div>
+       
+    )
+}
+
+export default Dashboard 
