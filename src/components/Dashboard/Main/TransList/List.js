7053e48806079e499a109f3b23c86eec0249d1dd
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-07-23 20:00:16 +0530

    feat - Adding the Transcontext

---
+++ src/components/Dashboard/Main/TransList/List.js
@@ -0,0 +1,65 @@
+import React,{useContext} from 'react'
+import { List as TransList,ListItem,ListItemAvatar,ListItemText,Avatar,ListItemSecondaryAction,IconButton,Slide} from '@material-ui/core'
+import { makeStyles } from "@material-ui/core/styles";
+import {Delete ,MoneyOff} from '@material-ui/icons';
+import { useTrans }  from '../../../../Context/TransContext';
+
+
+
+const useStyles = makeStyles((theme) => ({
+
+    avatarIncome: {
+    color: '#fff',
+    backgroundColor: 'green',
+  },
+  avatarExpense: {
+  
+    backgroundColor: 'red',
+  },
+  list: {
+    maxHeight: '150px',
+    overflow: 'auto',
+  },
+}))
+
+const List = () => {
+
+     const classes = useStyles();
+      
+      const {DeleteTransaction,AddTransaction}  = useTrans();
+
+     
+     const transactions= [
+
+       { id:1,type:'Income', category:'Salary',amount:'50',date:'11 May'},
+       { id:1,type:'Income', category:'Salary',amount:50,date:'11 May'},
+       { id:1,type:'Income', category:'Salary',amount:50,date:'11 May'},
+       { id:1,type:'Expense', category:'Sex',amount:20,date:'11 May'},
+       { id:1,type:'Income', category:'Shivani ',amount:50,date:'11 May'},
+       { id:1,type:'Income', category:'Shivani ',amount:50,date:'11 May'}
+
+     ]
+     return (
+         <TransList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete">
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </TransList>
+    )
+}
+
+export default List
