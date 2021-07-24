712c0d2358202de24cd8e7e672156e0901b4e86c
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-07-24 12:31:23 +0530

    feat- adding the reducer logic for adding and deleting transaction
    using High order functions

--- src/Context/ContextReducer.js
+++ src/Context/ContextReducer.js
@@ -1,14 +1,25 @@
 //this will  take previous state and action and then perform action according to it
 const ContextReducer = (state,action) => {
      
-    //  switch () {
-    //      case 'ADD_TRANSACTION':
+     let transactions;
+     switch (action.payload) {
+         case 'ADD_TRANSACTION':
              
-    //          break;
+             transactions =  [action.payload,state];
+             break;
+
+
+         case 'Delete_TRANSACTION':
+             
+             transactions = state.filter((t) => t.id != action.payload)  //this higher order function will remove the id matching id passed to be delted
+              //we have used here state because we set the state to be transaction
+            
+            return transactions
+             break;
      
-    //      default:
-    //          break;
-    //  }
+         default:
+             break;
+     }
 }
 
 export default ContextReducer
