012faa501295e1c55a943cd32725ea5da0fa5b0e
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-07-24 17:31:18 +0530

    fix - adding the default case

--- src/Context/ContextReducer.js
+++ src/Context/ContextReducer.js
@@ -1,25 +1,29 @@
 //this will  take previous state and action and then perform action according to it
+
+
+
 const ContextReducer = (state,action) => {
-     
+      
      let transactions;
-     switch (action.payload) {
+     switch (action.type) {
          case 'ADD_TRANSACTION':
              
-             transactions =  [action.payload,state];
-             break;
-
+             transactions =  [action.payload, ...state]
+              return transactions;
+      
 
          case 'Delete_TRANSACTION':
              
              transactions = state.filter((t) => t.id != action.payload)  //this higher order function will remove the id matching id passed to be delted
               //we have used here state because we set the state to be transaction
             
-            return transactions
-             break;
-     
-         default:
-             break;
+            return transactions;
+       default:
+          return state;
+          
      }
 }
 
 export default ContextReducer
+
+
