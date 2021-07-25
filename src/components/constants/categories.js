c2575001ef4c27d4db306cd3cc2ce0813fb5dbde
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-07-25 18:27:53 +0530

    fix- fixed the right order for the categories

--- src/components/constants/categories.js
+++ src/components/constants/categories.js
@@ -4,7 +4,7 @@ const expenseColors = ['#b50d12', '#bf2f1f', '#c9452c', '#d3583a', '#dc6a48', '#
 export const incomeCategories = [
   { type: 'Business', amount: 0, color: incomeColors[0] },
   { type: 'Investments', amount: 0, color: incomeColors[1] },
-  { type: 'Coupons,Checks', amount: 0, color: incomeColors[2] },
+  { type: 'Extra income', amount: 0, color: incomeColors[2] },
   { type: 'Deposits', amount: 0, color: incomeColors[3] },
   { type: 'Lottery', amount: 0, color: incomeColors[4] },
   { type: 'Gifts', amount: 0, color: incomeColors[5] },
@@ -15,19 +15,17 @@ export const incomeCategories = [
 
 export const expenseCategories = [
   { type: 'Bills', amount: 0, color: expenseColors[0] },
+  { type: 'Car', amount: 0, color: expenseColors[1] },
   { type: 'Clothes', amount: 0, color: expenseColors[2] },
   { type: 'Travel', amount: 0, color: expenseColors[3] },
-  { type: 'Netflix & Entertainment', amount: 0, color: expenseColors[7] },
-  { type: 'Car', amount: 0, color: expenseColors[1] },
   { type: 'Food', amount: 0, color: expenseColors[4] },
   { type: 'Shopping', amount: 0, color: expenseColors[5] },
   { type: 'House', amount: 0, color: expenseColors[6] },
+  { type: 'Entertainment', amount: 0, color: expenseColors[7] },
   { type: 'Phone', amount: 0, color: expenseColors[8] },
-  { type: 'Financial Expenses', amount: 0, color: expenseColors[8] },
   { type: 'Pets', amount: 0, color: expenseColors[9] },
   { type: 'Other', amount: 0, color: expenseColors[10] },
 ];
-
 
 export const resetCategories = () => {
   incomeCategories.forEach((c) => c.amount = 0);
