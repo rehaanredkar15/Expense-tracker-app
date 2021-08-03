0a68c0dcc65e749f08e353e4f5c19c2ef0665586
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-08-03 23:04:00 +0530

    feat- adding the reducer function for state management

--- src/Context/ContextReducer.js
+++ src/Context/ContextReducer.js
@@ -1,15 +1,10 @@
-//this will  take previous state and action and then perform action according to it
+// //this will  take previous state and action and then perform action according to it
+import {database } from '../firebase'
+import {useAuth} from './AuthContext';
 
 
 
 const ContextReducer = (state,action) => {
-      
-     let transactions;
-     switch (action.type) {
-         case 'ADD_TRANSACTION':
-             
-             transactions =  [action.payload, ...state]
-              return transactions;
       
 
          case 'DELETE_TRANSACTION':
@@ -25,5 +20,67 @@ const ContextReducer = (state,action) => {
 }
 
 export default ContextReducer
+
+
+
+//       const {CurrentUser} = useAuth();
+// //      let transactions;
+//      switch (action.type) {
+//          case 'ADD_TRANSACTION':
+//                return {
+//             ...state,
+//             transactions: [action.payload, ...state.transactions]
+//           }
+//             //  const TransactionDetails = action.payload;
+//             //  transactions =  [action.payload, ...state]
+              
+          
+//             //   database.users.doc(CurrentUser.uid).collection('transactions').add({
+
+//             //       ...transactions,
+//             //        userId:CurrentUser.uid,
+//             //   })
+              
+//               // return transactions;
+      
+
+//          case 'DELETE_TRANSACTION':
+
+//             return {
+//            ...state,
+//              transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+//            }
+// //              transactions = state.filter((t) => t.id != action.payload)  //this higher order function will remove the id matching id passed to be delted
+// //               //we have used here state because we set the state to be transaction    
+// //           //     try {            
+// //           //     database.users
+// //           //    .doc(CurrentUser.uid)
+// //           //    .collection('transactions')
+// //           //     .doc()
+// //           //     .delete()
+// //           //     .then( ()=>{
+// //           //          console.log('bhai ne bola krneka toh karneka')
+// //           //     })
+// //           //     } catch (err) {
+// //           //          console.log(err)
+// //           //          console.log('not done ')
+// //           //     }
+//             // return transactions;
+
+
+
+//           case 'GET_TRANSACTIONS':
+//           console.log(action.payload)
+//            return {
+//                  ...state,
+//              transactions: action.payload
+//            }
+
+           
+//        default:
+//           return state;
+          
+//      }
+
 
 
