d7c81b17eff7314b86052c45b328e3c938d8b2ae
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-08-03 23:02:46 +0530

    feat - Adding the context functions

--- src/Context/TransContext.js
+++ src/Context/TransContext.js
@@ -1,9 +1,16 @@
-import React,{useContext,useReducer} from 'react'
+import React,{useContext,useReducer,useEffect,useState} from 'react'
 import ContextReducer from './ContextReducer'
-const InitialState = [];
+import {database } from '../firebase'
+import {useAuth} from './AuthContext';
 
+const InitialState =  {
 
- const TransContext = React.createContext();
+     transactions:[],
+     loading: true,
+}
+
+ export const TransContext = React.createContext(InitialState);
+
 
 
 export function useTrans(){
@@ -15,33 +22,162 @@ export function useTrans(){
 
 export  function TransProvider({children})  {
 
+     var search;
+     let res = [];
+     let arr = [];
+     const [state, dispatch] = useReducer(ContextReducer, InitialState)
+     const [Spells,SetSpells] = useState('')
+       const {CurrentUser} = useAuth();    
+       
+       
 
-     const [transactions, dispatch] = useReducer(ContextReducer, InitialState)
-     
-     
-     
+
+
+
      //action creators
-     const DeleteTransaction =(id) =>{
+     async function DeleteTransaction(id){
 
-
-          dispatch({type : 'DELETE_TRANSACTION',
-          payload:id,
+        
+          try {
+                    const snapshot =  await database.users
+                                    .doc(CurrentUser.uid)
+                                    .collection('transactions')
+                                     .doc(id)
+                                     .delete()
+                                     .then(() => {
+                                          console.log("hey Dev !!deleted sucessfullly")
+                                     })
+                    
+                     } catch (err) {
+                               console.log(err)
+                    
+                     }
+        
+   
+                dispatch({type : 'DELETE_TRANSACTION',
+                  payload:id,
           })
      }
+     
+     async function SearchTransaction(id){
 
-      const AddTransaction = (transaction) =>{
+          try {
+                    SetSpells(await database.users
+                                    .doc(CurrentUser.uid)
+                                    .collection('transactions')
+                                     .doc(id)
+                                     .get());
+            
+                  
+          } catch (err) {
+               
+               console.log(err.message)
+          }
+     }
+    
+      async function UpdateTransaction(transaction,id){
+
+              try{
+                 const snapshot = await database.users.doc(CurrentUser.uid)
+                                     .collection('transactions')
+                                     .doc(id)
+                                     .get()
+                    snapshot.ref.update(transaction);
+                
+           }catch(err){
+
+                console.log(err.message)
+           }
         
+      }
+   
+
+      async function AddTransaction(transaction,id){
+           try{                                                                                   
+                await database.users.doc(CurrentUser.uid).collection('transactions').doc(id).set ({
+
+                   ...transaction,
+                   userId:CurrentUser.uid,
+               })
+            dispatch({
+                 type:'ADD_TRANSACTION',
+                 payload:transaction,
+           })
+           }catch(err){
+
+                console.log(err.message)
+           }
         
-        dispatch({
-             type:'ADD_TRANSACTION',
-             payload:transaction,
-        })
+     
         
      }
+
+        
+     //  async function  GetTransaction(id){
+        
+     //       try{
+     //            await database.users
+     //            .doc(CurrentUser.uid)
+     //            .collection('transactions')
+     //            .where("userId", "==",CurrentUser.uid)
+                
+     //            .onSnapshot((snapshot) => {
+            
+     //                     let changes = snapshot.docChanges();
+                       
+     //                    changes.forEach(change => {
+                          
+     //                      res.push(change.doc.data());
+                     
+     //                     })  
+     //           });
+              
+     //              console.log(res)
+     //            dispatch({
+     //            type:'FETCH_TRANSACTIONS',
+     //            payload:res,
+     //         })
+     //       }catch(err){
+
+     //            console.log(err.message)
+     //       }
+
+     // }
+ 
+
+
+     async function  FetchTransaction(id){
+        
+           try{
+                await database.users
+                .doc(CurrentUser.uid)
+                .collection('transactions')
+                .where("userId", "==",CurrentUser.uid)
+                .get()
+                .then((snapshot) => {              
+                 snapshot.docs.forEach(doc => {
+
+                          res.push(doc.data())
+                    
+                 })              
+               })
+                dispatch({
+                type:'GET_TRANSACTIONS',
+                payload:res,
+             })
+           }catch(err){
+
+                console.log(err.message)
+           }
+
+     }
+ 
+
      
-     const balance = transactions.reduce((acc,currval) => currval.type === 'Expense'? acc - currval.amount : acc + currval.amount,0);
+   
+     const balance = state.transactions.reduce((acc,currval) => currval.type === 'Expense'? acc - currval.amount : acc + currval.amount,0);
      const value={
-          DeleteTransaction,AddTransaction,transactions,balance
+          DeleteTransaction,AddTransaction, UpdateTransaction, transactions: state.transactions,balance,FetchTransaction,SearchTransaction,Spells
      }
     
     return (
@@ -52,3 +188,4 @@ export  function TransProvider({children})  {
 }
 
 export default TransContext
+
